1c4aa428bb47caa1ef385919d0811e389594b8bd
rafagars <rafagar96@gmail.com>
2019-10-10 17:40:53 -0400

    Authentication added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,54 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
+  include UsersHelper
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to Library App"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books.paginate(page: params[:page])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password)
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
